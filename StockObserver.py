ae798c5a9a178d9ac4c7ed476d5711110fdc3a07
sunwj <934962711@qq.com>
2020-10-22 15:03:26 +0800

    增加基金爬取相关内容

--- StockObserver.py
+++ StockObserver.py
@@ -6,8 +6,8 @@ var = 1
 
 print("价格监控开始...")
 while var == 1:
-    # 这里表示10秒钟循环一次
-    time.sleep(10)
+    # 这里表示3秒钟循环一次
+    time.sleep(3)
     stock_list = ['600258', '600036','512800','000002','601933','600600']
     df = ts.get_realtime_quotes(stock_list)
     # 首旅酒店
@@ -20,7 +20,7 @@ while var == 1:
     if float(df[df['code'] == '512800']['price']) <= 1.06:
         print(df[df['code'] == '512800'])
     # 万科A
-    if float(df[df['code'] == '000002']['price']) <= 24.9:
+    if float(df[df['code'] == '000002']['price']) <= 27:
         print(df[df['code'] == '000002'])
     # 永辉超市(7.9进入观察，买点在7.5以下)
     if float(df[df['code'] == '601933']['price']) <= 7.9:
